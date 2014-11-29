02403454fa983e7c39164ad6e888bf500030e4c1
Karl Zylinski <karl@zylinski.se>
2014-11-29 23:05:27 +0100

    Snurr!

---
+++ Assets/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour
+{
+    public float Speed = 3.0f;
+
+    public void Update()
+    {
+        transform.Rotate(0, 0, Time.deltaTime * Speed);
+    }
+}
