f281752764477f10e1f7df3eccfc9117756e36d1
Tobias Möllstam <tobias@mollstam.com>
2014-11-30 16:24:52 +0100

    added main menu with jupiter!

---
+++ Assets/JupiterController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JupiterController : MonoBehaviour {
+
+    public float RotationSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 rot = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z);
+        rot.y += RotationSpeed * Time.deltaTime;
+        transform.eulerAngles = rot;
+	}
+}
